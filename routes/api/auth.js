dbff98be95ff798ddcdb1911501f9b45018af6e2
Edandaveywood <Edandaveywood@gmail.com>
2021-08-14 19:41:31 +1200

    Post API routes   - Create post and comments   - Delete posts and comments   - Like and unlike posts

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -42,13 +42,17 @@ router.post(
 			let user = await User.findOne({ email });
 
 			if (!user) {
-				return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "Invalid Credentials" }] });
 			}
 
 			const isMatch = await bcrypt.compare(password, user.password);
 
 			if (!isMatch) {
-				return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "Invalid Credentials" }] });
 			}
 
 			// Return JSON webtoken
--- routes/api/users.js
+++ routes/api/users.js
@@ -12,70 +12,70 @@ const User = require("../../models/User");
 // @desc    Register user
 // @access  Public
 router.post(
-  "/",
-  [
-    check("name", "Name is required").not().isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
-    check("password", "Please enter a password").isLength({ min: 6 }),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const { name, email, password } = req.body;
+	"/",
+	[
+		check("name", "Name is required").not().isEmpty(),
+		check("email", "Please include a valid email").isEmail(),
+		check("password", "Please enter a password").isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const { name, email, password } = req.body;
 
-    try {
-      // See if user exists
-      let user = await User.findOne({ email });
+		try {
+			// See if user exists
+			let user = await User.findOne({ email });
 
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "User already exists" }] });
-      }
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "User already exists" }] });
+			}
 
-      // Get users gravatar
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm",
-      });
+			// Get users gravatar
+			const avatar = gravatar.url(email, {
+				s: "200",
+				r: "pg",
+				d: "mm",
+			});
 
-      user = new User({
-        name,
-        email,
-        avatar,
-        password,
-      });
-      // Encrypt password
-      const salt = await bcrypt.genSalt(10);
+			user = new User({
+				name,
+				email,
+				avatar,
+				password,
+			});
+			// Encrypt password
+			const salt = await bcrypt.genSalt(10);
 
-      user.password = await bcrypt.hash(password, salt);
+			user.password = await bcrypt.hash(password, salt);
 
-      await user.save();
+			await user.save();
 
-      // Return JSON webtoken
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
+			// Return JSON webtoken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
 
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server error");
-    }
-  }
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Server error");
+		}
+	}
 );
 
 module.exports = router;
