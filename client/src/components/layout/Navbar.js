352648a94bd5d047bbff9cf3e70e427c516df2fe
Edandaveywood <Edandaveywood@gmail.com>
2021-08-27 03:27:50 +1200

    Display profiles on Developers page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,13 +8,16 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const authLinks = (
 		<ul>
 			<li>
+				<Link to='/profiles'>Developers</Link>
+			</li>
+			<li>
 				<Link to='/dashboard'>
 					<i className='fas fa-user'> </i>{" "}
 					<span className='hide-sm'>Dashboard</span>
 				</Link>
 			</li>
 			<li>
-				<a onClick={logout} href='#!'>
+				<a onClick={logout}>
 					<i className='fas fa-sign-out-alt'> </i>{" "}
 					<span className='hide-sm'> Logout </span>
 				</a>
@@ -24,7 +27,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const guestLinks = (
 		<ul>
 			<li>
-				<Link to='#!'>Developers</Link>
+				<Link to='/profiles'>Developers</Link>
 			</li>
 			<li>
 				<Link to='/register'>Register</Link>
