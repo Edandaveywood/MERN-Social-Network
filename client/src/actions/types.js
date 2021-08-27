b6cf5f96d8230bf57ccb4d94fd8a3c09ff3c6455
Edandaveywood <Edandaveywood@gmail.com>
2021-08-27 16:09:07 +1200

    Deleting a post

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -13,6 +13,7 @@ export const AUTH_ERROR = "AUTH_ERROR";
 
 export const GET_POSTS = "GET_POSTS";
 export const UPDATE_LIKES = "UPDATE_LIKES";
+export const DELETE_POST = "DELETE_POSTS";
 export const POST_ERROR = "POST_ERROR";
 
 export const GET_PROFILE = "GET_PROFILE";
